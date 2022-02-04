bcbc7944f2bc0507ace8a3c175fe11e265081493
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-04 17:39:39 +0545

    donor routes added

---
+++ modules/donor/donor.routes.js
@@ -0,0 +1,27 @@
+const validators = require('./donor.validators');
+const controllers = require('./donor.controllers');
+
+const routes = {
+	add: ['POST', '', 'Add donor'],
+	list: ['GET', '', 'List all donors'],
+	getById: ['GET', '/:id', 'Find donor by ID'],
+	getByPhone: ['GET', '/byphone', 'Find donor by phone number'],
+	getByName: ['GET', '/byname', 'Find donor by name'],
+	update: ['PUT', '/:id', 'Update a donor'],
+	remove: ['DELETE', '/:id', 'Remove a donor'],
+};
+
+/**
+ * Register the routes
+ * @param {object} app Application.
+ */
+function register(app) {
+	app.register({
+		name: 'donors',
+		routes,
+		validators,
+		controllers,
+	});
+}
+
+module.exports = register;
