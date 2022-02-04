fec108ec816171568f0dc4efb450e03f3c7af583
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-04 15:08:02 +0545

    donor model created

---
+++ modules/donors/donor.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema;
+
+const Schema = mongoose.Schema(
+	{
+		donor_no: { type: String }, // HLB001
+		name: { type: String, required: true },
+		phone: { type: String, required: true },
+		contacts: [{ phone: String }],
+		email: { type: String },
+		address: { type: String },
+		dob: { type: Date },
+		gender: { type: String, required: true, enum: ['M', 'F', 'O'] },
+		blood_info: {
+			group: { type: String, enum: ['A', 'B', 'O', 'AB', ''] },
+			rh_factor: { type: String, enum: ['+', '-'] },
+			is_verified: { type: Boolean, required: true, default: false },
+			verified_date: { type: Date },
+		},
+		last_donated_date: { type: Date },
+		donations_legacy: [{ date: Date, site: String }],
+		geo_location: {
+			longitude: Number,
+			latitude: Number,
+		},
+		source: {
+			name: String,
+		},
+		extras: {},
+		created_by: { type: ObjectId, ref: 'User' },
+		updated_by: { type: ObjectId, ref: 'User' },
+	},
+	{
+		collection: 'donors',
+		timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+		toObject: { virtuals: true },
+		toJSON: { virtuals: true },
+	},
+);
+
+module.exports = mongoose.model('Donor', Schema);
