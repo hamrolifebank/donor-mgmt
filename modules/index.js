ccb8997bd6fde0ec3360d7847cee2a6f76b3ebf4
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-04 17:40:26 +0545

    adding routes and controllers to index file

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,9 @@
 const { Role } = require('./user/role.controllers');
 const { User } = require('./user/user.controllers');
+const { Donor } = require('./donor/donor.controllers');
 
 module.exports = {
 	Role,
-	User
+	User,
+	Donor,
 };
--- modules/routes.js
+++ modules/routes.js
@@ -1,9 +1,11 @@
 const auth = require('./user/auth.routes');
 const role = require('./user/role.routes');
 const user = require('./user/user.routes');
+const donor = require('./donor/donor.routes');
 
 module.exports = {
 	auth,
 	role,
-	user
+	user,
+	donor,
 };
