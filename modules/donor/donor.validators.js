22f69fb1433d5c89f2c8fa7dc1193e4ef36a7559
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-04 20:07:04 +0545

    update and remove validations added

--- modules/donor/donor.validators.js
+++ modules/donor/donor.validators.js
@@ -44,4 +44,35 @@ module.exports = {
 	getById: {
 		params: GooseJoi.id('Enter Donor ID'),
 	},
+
+	remove: {
+		params: GooseJoi.id('Enter Donor ID'),
+	},
+
+	update: {
+		params: GooseJoi.id('Enter Donor ID'),
+		payload: Joi.object({
+			name: Joi.string(),
+			phone: Joi.string().example('014223232 / 9841232323'),
+			contacts: Joi.array().items(Joi.object({ phone: Joi.string().allow('') })),
+			email: Joi.string().allow('').example('hello@gmail.com'),
+			address: Joi.string().allow('').example('Kathmandu'),
+			dob: Joi.date().allow(''),
+			gender: Joi.string().allow('').example('M / F / O'),
+			blood_info: Joi.object({
+				group: Joi.string().example('A / B / AB / O'),
+				rh_factor: Joi.string().example('- / +'),
+				is_verified: Joi.boolean(),
+				verified_date: Joi.date(),
+			}).allow(''),
+			last_donated_date: Joi.date().allow(''),
+			donationsLegacy: Joi.array().items(Joi.date()).allow(''),
+			geo_location: Joi.object({
+				longitude: Joi.number(),
+				latitude: Joi.number(),
+			}).allow(''),
+			source: Joi.string().allow(''),
+			extras: Joi.object().allow(''),
+		}).label('Donor'),
+	},
 };
