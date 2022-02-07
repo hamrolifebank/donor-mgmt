f279e41b4d5521140fb3aa967147f637c633bbd6
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-07 10:11:06 +0545

    routes updated

--- modules/donor/donor.routes.js
+++ modules/donor/donor.routes.js
@@ -3,12 +3,16 @@ const controllers = require('./donor.controllers');
 
 const routes = {
 	add: ['POST', '', 'Add donor'],
+	addSingleVerified: ['POST', '/add-single-verified', 'Add single verified'],
 	list: ['GET', '', 'List all donors'],
 	getById: ['GET', '/{id}', 'Find donor by ID'],
 	getByPhone: ['GET', '/byphone', 'Find donor by phone number'],
 	getByName: ['GET', '/byname', 'Find donor by name'],
 	update: ['PUT', '/{id}', 'Update a donor'],
 	remove: ['DELETE', '/{id}', 'Remove a donor'],
+	addDonation: ['POST', '/{id}/donations', 'Add donations'],
+	removeDonation: ['DELETE', '/{id}/donations', 'Remove donations'],
+	dispatch: ['GET', '/dispatch/{id}', 'Dispatch'],
 };
 
 /**
