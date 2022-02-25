b7c8030f38ded35f6884e0924883dc132e05f78b
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-02-25 15:16:14 +0545

    event routes added

---
+++ modules/events/event.routes.js
@@ -0,0 +1,32 @@
+const validators = require('./event.validators');
+const controllers = require('./event.controllers');
+
+const routes = {
+	add: ['POST', '', 'Add Event'],
+	list: ['GET', '', 'List all Events'],
+	getById: ['GET', '/{eventId}', 'Get event by ID'],
+	createAndSendCertificates: ['GET', '/{eventId}/certificates', 'Create and send certificates'],
+	update: ['PUT', '/{eventId}', 'Update an Event'],
+	getEventUsers: ['GET', '/{eventId}', 'Get users registered to an event'],
+	addEventUsers: ['POST', '/{eventId}', 'Add users to an event'],
+	inviteUsers: ['POST', '/{eventId}/invite', 'Invite users to an event'],
+	listEventWithDetails: ['GET', '/{eventId}/donors', 'Get event details'],
+	getRegisterOption: ['GET', '/{eventId}/register', 'Get register options'],
+	register: ['POST', '/{eventId}/register', 'Register anyone to an event'],
+	check: ['GET', '/{eventId}/check', 'Check bloodbag or tubeID'],
+};
+
+/**
+ * Register the routes
+ * @param {object} app Application.
+ */
+function register(app) {
+	app.register({
+		name: 'donors',
+		routes,
+		validators,
+		controllers,
+	});
+}
+
+module.exports = register;
