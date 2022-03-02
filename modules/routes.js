949ac2b01a38b2e110f8ee2fa2ee6c25ee48de5f
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-03-02 20:23:11 +0545

    organization route added

--- modules/routes.js
+++ modules/routes.js
@@ -3,6 +3,7 @@ const role = require('./user/role.routes');
 const user = require('./user/user.routes');
 const donor = require('./donor/donor.routes');
 const event = require('./events/event.routes');
+const organization = require('./organization/organization.routes');
 
 module.exports = {
 	auth,
@@ -10,4 +11,5 @@ module.exports = {
 	user,
 	donor,
 	event,
+	organization,
 };
