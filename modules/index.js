96831fccfa4eaaf0a23ab93972e32fb5b7596b8f
Prithvi Maharjan <maharjanprithvi@gmail.com>
2022-03-03 16:20:13 +0545

    Organization controller exported

--- modules/index.js
+++ modules/index.js
@@ -2,10 +2,12 @@ const { Role } = require('./user/role.controllers');
 const { User } = require('./user/user.controllers');
 const { Donor } = require('./donor/donor.controllers');
 const { Event } = require('./events/event.controllers');
+const { Organization } = require('./organization/organization.controllers');
 
 module.exports = {
 	Role,
 	User,
 	Donor,
 	Event,
+	Organization,
 };
